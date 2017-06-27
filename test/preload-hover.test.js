eb33d7478146cb22368689802da015462ff365f9
Andrew Rady <andrew.rady@ritterim.com>
2017-06-27 16:11:26 -0600

    tested PreloadHover basic function

--- test/preload-hover.test.js
+++ test/preload-hover.test.js
@@ -8,3 +8,10 @@ beforeEach(() => {
 it('should construct', () => {
   new PreloadHover();
 });
+
+it('Creates link based on value passed from parameter', () => {
+  const defaultScope = [document.querySelector('#one')]
+  const preload = new PreloadHover({defaultDomScope: defaultScope});
+  expect(preload.configuration.defaultDomScope).toEqual(defaultScope);
+});
+
