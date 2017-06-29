840a939eee3ac11f73188292406bc01d29dfc872
Andrew Rady <andrew.rady@ritterim.com>
2017-06-29 12:41:05 -0600

    changed linkType to array with accepted strings and included in the conditionals

--- src/preload-hover.js
+++ src/preload-hover.js
@@ -5,8 +5,7 @@ export default class PreloadHover {
     const defaultConfiguration = {
       defaultDomScope: [document.body],
       debounceTime: 50,
-      linkType: true, 
-      mobile: false,
+      linkType: ['local', 'external', 'both'], 
     };
 
     this.configuration = defaultConfiguration;
@@ -18,45 +17,44 @@ export default class PreloadHover {
 
   start(domScopes = this.configuration.defaultDomScope) {
     if (!domScopes) { throw new Error('domScopes must be provided.'); }
-    
-    if(this.configuration.mobile === false) {
-      const head = document.getElementsByTagName('head')[0];
+    const head = document.getElementsByTagName('head')[0];
 
-      domScopes.forEach(domScope => {
-        const links = [...domScope.getElementsByTagName('a')];
-        let timer;
-        let uniqueLinks = uniqBy(links, 'href');
+    domScopes.forEach(domScope => {
+      const links = [...domScope.getElementsByTagName('a')];
+      let timer;
+      let uniqueLinks = uniqBy(links, 'href');
 
-        uniqueLinks.forEach(link => {
-          link.addEventListener('mouseover', () => {
-            if(this.configuration.linkType === true) {
+      uniqueLinks.forEach(link => {
+        link.addEventListener('mouseover', () => {
+          if(this.configuration.linkType === 'local' || this.configuration.linkType ==='both') {
+            timer = setTimeout(() => {
+              const preload = document.createElement('link');
+
+              preload.setAttribute('rel', 'preload');
+              preload.setAttribute('href', link.href);
+              head.appendChild(preload);
+              console.log(preload);
+            }, this.configuration.debounceTime);
+          } else if(this.configuration.linkType === 'external') {
+            if(link.hostname != window.location.hostname){
               timer = setTimeout(() => {
                 const preload = document.createElement('link');
 
                 preload.setAttribute('rel', 'preload');
                 preload.setAttribute('href', link.href);
+
                 head.appendChild(preload);
+                console.log(preload);
               }, this.configuration.debounceTime);
-            } else {
-              if(link.hostname != window.location.hostname) {
-                timer = setTimeout(() => {
-                  const preload = document.createElement('link');
-
-                  preload.setAttribute('rel', 'preload');
-                  preload.setAttribute('href', link.href);
-
-                  head.appendChild(preload);
-                }, this.configuration.debounceTime);
-              }
             }
-          });
+          }
+        });
 
-          link.addEventListener('mouseout', () => {
-            clearTimeout(timer);
-          });
+        link.addEventListener('mouseout', () => {
+          clearTimeout(timer);
         });
       });
-    }
+    });
 
   }
 }
