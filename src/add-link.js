939b3682d3cae6171429c64486dd92ac030fb274
Andrew Rady <andrew.rady@ritterim.com>
2017-07-05 09:52:19 -0600

    moved add link function back to inline within the html for demo and removed external file

--- src/add-link.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('add-link').addEventListener('click', () => {
-  const a = document.createElement('a');
-        const link = document.createTextNode('Title');
-        a.appendChild(link);
-        a.title = 'New Button';
-        a.href = 'https://www.facebook.com';
-        document.getElementById('one').appendChild(a);
-});
